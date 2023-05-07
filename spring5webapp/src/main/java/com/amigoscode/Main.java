45b40f9147e249419974f7aeedc3f469f20e0ec8
akash <akash19102001@gmail.com>
2023-05-07 12:12:25 +0530

    class commit

--- spring5webapp/src/main/java/com/amigoscode/Main.java
+++ spring5webapp/src/main/java/com/amigoscode/Main.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @SpringBootApplication
 @RestController
 public class Main {
@@ -13,7 +15,12 @@ public class Main {
         SpringApplication.run(Main.class,args);
     }
 @GetMapping("/greet")
-    public String greet(){
-        return "hello";
+    public GreetResponse greet(){
+        return new GreetResponse("hello",List.of("java","python","c"),new Person("abhay"));
+    }
+    record Person(String name){
+
+    }
+    record GreetResponse(String greet, List<String> favprogramminglanguages,Person person){
     }
 }
